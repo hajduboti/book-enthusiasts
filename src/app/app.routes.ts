333541bf29f2cd0ae39d60f4fcd0231ccd503628
bthmono <70575086+bthmono@users.noreply.github.com>
2024-12-19 13:23:33 +0100

    routes and component skeletons

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { LandingComponent } from './landing/landing.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: LandingComponent
+  },
+  {
+    path: '**',
+    component: PageNotFoundComponent
+  }
+];
